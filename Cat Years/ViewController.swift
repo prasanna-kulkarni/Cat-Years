f4ceca4229cd64cbbef1317496ed1d3d1455771d
Prasanna Sunil Kulkarni <prasannakulkarni@live.com>
2015-02-07 17:39:14 -0500

    Built with Xcode

--- Cat Years/ViewController.swift
+++ Cat Years/ViewController.swift
@@ -9,6 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var catAge: UITextField!
+    
+    
+    @IBOutlet weak var Message: UILabel!
+    
+    
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        
+        var age = catAge.text.toInt()
+        
+        age = age! * 7
+        
+        
+        Message.text = "Your cat is \(age!)years old"
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
